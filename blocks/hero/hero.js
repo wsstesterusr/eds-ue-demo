88d997fa4ddac4c2082e9f0e400e59f7ef61db29
wsstesterusr <winicius.silva@vilt-group.com>
2025-08-12 16:45:21 -0300

    Update hero.js

---
+++ blocks/hero/hero.js
@@ -0,0 +1,19 @@
+import { decodeBase64, htmlToElement } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+
+  const header = () => {
+
+    return (`
+            <section class="olho-with-title">
+                <div>
+                  <h4><strong>Olá</strong></h4>
+                </div>
+                <div>
+                  <p>Estou aqui!</p>
+                </div>
+            </section>
+        `);
+  };
+  block.innerHTML = header();
+}
