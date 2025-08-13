a4d32e6341c266a35d7d67931487474b01a7b544
wsstesterusr <winicius.silva@vilt-group.com>
2025-08-13 01:54:37 -0300

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -10,7 +10,7 @@ export default function decorate(block) {
   const descriptionText = description?.textContent?.trim();
 
   console.log(image.innerHTML);
-  image.innerHTML.querySelector('img')?.classList.add('d-block', 'w-100');
+  image.querySelector('img')?.classList.add('d-block', 'w-100');
 
   const header = () => {
 
@@ -62,6 +62,7 @@ export default function decorate(block) {
   };
   block.innerHTML = header();
 }
+
 
 
 
